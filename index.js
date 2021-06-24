0fe59c724fe718247c3fb3032b78bb67e7c875d4
will14lee <will14lee@gmail.com>
2021-06-24 16:42:20 -0400

    commit tree 468105829d6f41c20422d4b518dffe865570d52b

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Add your code here
+let nameButt=document.querySelector("button#nameButton")
+let mailButt=document.querySelector("button#mailButton")
+nameButt.addEventListener("click", ()=>{
+    let nameInput=document.querySelector("input#name")
+    let pName= document.querySelector("p#pName")
+    if(nameInput.value!==""){
+    pName.innerHTML=nameInput.value
+    }
+    else{
+        alert("Please type a name!")
+    }
+});
+mailButt.addEventListener("click", ()=>{
+    let mailInput=document.querySelector("input#email")
+    let mMail= document.querySelector("p#pMail")
+    if(mailInput.value!==""){
+    mMail.innerHTML=mailInput.value
+    }
+    else{
+        alert("Please type an email")
+    }
+})
+
+const data={
+    dogName: "Bryon",
+    dogBreed: "Poodle"
+};
+const configurationObject= {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+};
+
+fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        console.log(object);
+    })
+    .catch(function(error){
+        alert("Cease and Desist!");
+        console.log(error.message);
+    });
+
